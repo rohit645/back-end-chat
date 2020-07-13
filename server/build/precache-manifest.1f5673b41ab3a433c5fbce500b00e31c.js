22a55928defc635cea019b18634aa343e9bb93b1
rohit645 <kumarnath.rohit@gmail.com>
2020-07-13 18:08:57 +0530

    first one

---
+++ server/build/precache-manifest.1f5673b41ab3a433c5fbce500b00e31c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0327abd0ede4eda2bf75",
+    "url": "/static/js/main.0327abd0.chunk.js"
+  },
+  {
+    "revision": "b9fbd45a7dc0e487a351",
+    "url": "/static/js/1.b9fbd45a.chunk.js"
+  },
+  {
+    "revision": "0327abd0ede4eda2bf75",
+    "url": "/static/css/main.b3bfd859.chunk.css"
+  },
+  {
+    "revision": "b9fbd45a7dc0e487a351",
+    "url": "/static/css/1.3c8fb60b.chunk.css"
+  },
+  {
+    "revision": "7a07b5b2d894d3b44b9c9ed7f8b9640a",
+    "url": "/index.html"
+  }
+];
